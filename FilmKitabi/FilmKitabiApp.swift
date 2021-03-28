7529ac6495c8a8b2429cbc2f86d58e490f572441
dikranteymur <mdteymur@gmail.com>
2021-03-28 17:06:42 +0300

    Initial Commit

---
+++ FilmKitabi/FilmKitabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmKitabiApp.swift
+//  FilmKitabi
+//
+//  Created by mdt on 28.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct FilmKitabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
